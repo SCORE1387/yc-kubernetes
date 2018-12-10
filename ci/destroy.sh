4b8025df9f3866167480bb3c948c287e199cbfbb
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2018-12-10 22:25:05 +0300

    destroy.sh

---
+++ ci/destroy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+~/yandex-cloud/bin/yc compute instance delete master01
+~/yandex-cloud/bin/yc compute instance delete master02
+~/yandex-cloud/bin/yc compute instance delete master03
+
