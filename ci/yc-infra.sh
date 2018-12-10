c222d654f13fc351292292926730f849b6ec5f6d
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 19:49:21 +0000

    Update yc-infra.sh

--- ci/yc-infra.sh
+++ ci/yc-infra.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
-
-deploy_master master01 ru-central1-a kube-subnet-a 192.168.10.3 
-deploy_master master02 ru-central1-b kube-subnet-b 192.168.20.3 
-deploy_master master03 ru-central1-c kube-subnet-c 192.168.30.3 
-
-deploy_node node01 ru-central1-a kube-subnet-a 192.168.10.10
-deploy_node node02 ru-central1-b kube-subnet-b 192.168.20.10
-deploy_node node03 ru-central1-c kube-subnet-c 192.168.30.10 
-
 # $1 - vm name
 # $2 - zone
 # $3 - subnet name
@@ -73,3 +63,14 @@ function deploy_node {
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
 }
+
+
+deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
+
+deploy_master master01 ru-central1-a kube-subnet-a 192.168.10.3 
+deploy_master master02 ru-central1-b kube-subnet-b 192.168.20.3 
+deploy_master master03 ru-central1-c kube-subnet-c 192.168.30.3 
+
+deploy_node node01 ru-central1-a kube-subnet-a 192.168.10.10
+deploy_node node02 ru-central1-b kube-subnet-b 192.168.20.10
+deploy_node node03 ru-central1-c kube-subnet-c 192.168.30.10 
