29c597e42505dbba0eba3893b4f3f1376657136f
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 20:38:22 +0000

    [ci skip] Update yc-vpc.sh

--- ci/yc-vpc.sh
+++ ci/yc-vpc.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "Create network kube-net"
 yc vpc network create --name kube-net
 
+echo "Create subnet kube-subnet-a"
 yc vpc subnet create kube-subnet-a \
   --network-name kube-net \
   --zone ru-central1-a \
   --range 192.168.10.0/24
 
+echo "Create subnet kube-subnet-b"
 yc vpc subnet create kube-subnet-b \
   --network-name kube-net \
   --zone ru-central1-b \
   --range 192.168.20.0/24
 
+echo "Create subnet kube-subnet-c"
 yc vpc subnet create kube-subnet-c \
   --network-name kube-net \
   --zone ru-central1-c \
