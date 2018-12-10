5b4b705e2a1eaf5472c3b227ee705dcc9d4e9051
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 19:08:01 +0000

    Update yc-infra.sh

--- ci/yc-infra.sh
+++ ci/yc-infra.sh
@@ -7,7 +7,7 @@
     --zone ru-central1-a \
 	--memory 2 \
 	--cores 2 \
-    --network-interface subnet-name=kube-subnet-a,address=192.168.10.1 \
+    --network-interface subnet-name=kube-subnet-a,address=192.168.10.2 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
 	
@@ -18,17 +18,17 @@
     --zone ru-central1-b \
 	--memory 2 \
 	--cores 2 \
-    --network-interface subnet-name=kube-subnet-b,address=192.168.20.1 \
+    --network-interface subnet-name=kube-subnet-b,address=192.168.20.2 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
 
 # master03	
 ~/yandex-cloud/bin/yc compute instance create \
     --name master03 \
-	--hostname master01 \
+	--hostname master03 \
     --zone ru-central1-c \
 	--memory 2 \
 	--cores 2 \
-    --network-interface subnet-name=kube-subnet-c,address=192.168.30.1 \
+    --network-interface subnet-name=kube-subnet-c,address=192.168.30.2 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
