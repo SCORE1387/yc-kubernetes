fbfb0cad7913cee503a92ccdcd6ef55ab4269077
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 18:41:42 +0000

    Update yc-infra.sh

--- ci/yc-infra.sh
+++ ci/yc-infra.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo "infra"
+# master01
+yc compute instance create \
+    --name master01 \
+	--hostname master01 \
+    --zone ru-central1-a \
+	--memory 2 \
+	--cores 2 \
+    --public-ip \
+    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+    --ssh-key ./ci/id_rsa.pub
+	
+# master02
+yc compute instance create \
+    --name master01 \
+	--hostname master02 \
+    --zone ru-central1-b \
+	--memory 2 \
+	--cores 2 \
+    --public-ip \
+    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+    --ssh-key ./ci/id_rsa.pub
+
+# master03	
+yc compute instance create \
+    --name master03 \
+	--hostname master01 \
+    --zone ru-central1-c \
+	--memory 2 \
+	--cores 2 \
+    --public-ip \
+    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+    --ssh-key ./ci/id_rsa.pub
