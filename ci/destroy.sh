756dc5af314dd54b72809ef1f4fe55f0e3f7c440
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 19:58:44 +0000

    Update destroy.sh

--- ci/destroy.sh
+++ ci/destroy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-~/yandex-cloud/bin/yc compute instance delete access
-
+echo "Destroy Node (node01) VM."
 ~/yandex-cloud/bin/yc compute instance delete node01
+echo "Destroy Node (node02) VM."
 ~/yandex-cloud/bin/yc compute instance delete node02
+echo "Destroy Node (node03) VM."
 ~/yandex-cloud/bin/yc compute instance delete node03
 
+echo "Destroy Master (master01) VM."
 ~/yandex-cloud/bin/yc compute instance delete master01
+echo "Destroy Master (master02) VM."
 ~/yandex-cloud/bin/yc compute instance delete master02
+echo "Destroy Master (master03) VM."
 ~/yandex-cloud/bin/yc compute instance delete master03
 
+echo "Destroy Access VM."
+~/yandex-cloud/bin/yc compute instance delete access
