6d91d5ef52a2584ffa63739f3e000ebe20edd6b1
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 20:40:28 +0000

    Update yc-vpc.sh

--- ci/yc-vpc.sh
+++ ci/yc-vpc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Create network kube-net"
-yc vpc network create --name kube-net
+~/yandex-cloud/bin/yc vpc network create --name kube-net
 
 echo "Create subnet kube-subnet-a"
-yc vpc subnet create kube-subnet-a \
+~/yandex-cloud/bin/yc vpc subnet create kube-subnet-a \
   --network-name kube-net \
   --zone ru-central1-a \
   --range 192.168.10.0/24
 
 echo "Create subnet kube-subnet-b"
-yc vpc subnet create kube-subnet-b \
+~/yandex-cloud/bin/yc vpc subnet create kube-subnet-b \
   --network-name kube-net \
   --zone ru-central1-b \
   --range 192.168.20.0/24
 
 echo "Create subnet kube-subnet-c"
-yc vpc subnet create kube-subnet-c \
+~/yandex-cloud/bin/yc vpc subnet create kube-subnet-c \
   --network-name kube-net \
   --zone ru-central1-c \
   --range 192.168.30.0/24  
