8fb777a5ae75bcd358a328a98f7cdce5dd424595
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 19:17:52 +0000

    Update yc-infra.sh

--- ci/yc-infra.sh
+++ ci/yc-infra.sh
@@ -7,7 +7,7 @@
     --zone ru-central1-a \
     --memory 2 \
     --cores 2 \
-    --network-interface subnet-name=kube-subnet-a,address=192.168.10.2 \
+    --network-interface subnet-name=kube-subnet-a,address=192.168.10.3 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
 	
@@ -18,7 +18,7 @@
     --zone ru-central1-b \
     --memory 2 \
     --cores 2 \
-    --network-interface subnet-name=kube-subnet-b,address=192.168.20.2 \
+    --network-interface subnet-name=kube-subnet-b,address=192.168.20.3 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
 
@@ -29,6 +29,6 @@
     --zone ru-central1-c \
     --memory 2 \
     --cores 2 \
-    --network-interface subnet-name=kube-subnet-c,address=192.168.30.2 \
+    --network-interface subnet-name=kube-subnet-c,address=192.168.30.3 \
     --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
     --ssh-key ./ci/id_rsa.pub
