64733a34287d1facf1d00c7fbef53d92511647b4
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 19:46:57 +0000

    Update destroy.sh

--- ci/destroy.sh
+++ ci/destroy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+~/yandex-cloud/bin/yc compute instance delete access
+
+~/yandex-cloud/bin/yc compute instance delete node01
+~/yandex-cloud/bin/yc compute instance delete node02
+~/yandex-cloud/bin/yc compute instance delete node03
+
 ~/yandex-cloud/bin/yc compute instance delete master01
 ~/yandex-cloud/bin/yc compute instance delete master02
 ~/yandex-cloud/bin/yc compute instance delete master03
