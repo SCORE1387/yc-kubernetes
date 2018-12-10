06b3cb6c13c6cce66d268dd9d42063fe014b9b1f
Sergey Kolesnikov <kolesnik-mipt@phystech.edu>
2018-12-10 20:17:09 +0000

    Update yc-infra.sh

--- ci/yc-infra.sh
+++ ci/yc-infra.sh
@@ -67,9 +67,6 @@ echo "Start Node ${name} VM deployment."
     --ssh-key ./ci/id_rsa.pub
 }
 
-
-deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
-
 deploy_master master01 ru-central1-a kube-subnet-a 192.168.10.3 
 deploy_master master02 ru-central1-b kube-subnet-b 192.168.20.3 
 deploy_master master03 ru-central1-c kube-subnet-c 192.168.30.3 
@@ -77,3 +74,9 @@ deploy_master master03 ru-central1-c kube-subnet-c 192.168.30.3
 deploy_node node01 ru-central1-a kube-subnet-a 192.168.10.10
 deploy_node node02 ru-central1-b kube-subnet-b 192.168.20.10
 deploy_node node03 ru-central1-c kube-subnet-c 192.168.30.10 
+
+deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
+
+# Find out Access VM Public IP
+ACCESS_VM_PUBLIC_IP=$(yc compute instance get access --format json | jq .network_interfaces[0].primary_v4_address.one_to_one_nat.address)
+echo "Acess VM Public IP: ${ACCESS_VM_PUBLIC_IP}"
