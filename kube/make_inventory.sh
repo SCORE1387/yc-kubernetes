efccd4cbc4020f9b475157fba3d4a2ad2f118d49
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2018-12-15 13:21:42 +0300

    make kube inventory

---
+++ kube/make_inventory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $1 - vm name
+function make_ip {
+    local name=$1
+
+public_ip=$(~/yandex-cloud/bin/yc compute instance get ${name} --format json | jq .network_interfaces[0].primary_v4_address.one_to_one_nat.address)
+echo "${name} VM Public IP: ${public_ip}"
+
+sed -i -e "s/{{ ${name} }}/${public_ip}/g" kube_hosts.ini
+
+}
+
+make_ip master01
+make_ip master02
+make_ip master03
+
+make_ip node01
+make_ip node02
+make_ip node03
+
