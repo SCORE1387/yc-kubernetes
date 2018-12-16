d70c89340b87cd64962356e4f0e3ed99da892f52
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2018-12-16 22:00:51 +0300

    infra deployment idempotency

--- infra/yc-compute.sh
+++ infra/yc-compute.sh
@@ -11,16 +11,20 @@ function deploy_access_vm {
     local ip=$4
     
 echo "Start Access VM deployment."
-~/yandex-cloud/bin/yc compute instance create \
-    --name ${name} \
-    --hostname ${name} \
-    --zone ${zone} \
-    --memory 1 \
-    --cores 1 \
-    --core-fraction 5 \
-    --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4  \
-    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
-    --ssh-key ./infra/id_rsa.pub
+if ~/yandex-cloud/bin/yc compute instance get ${name} > /dev/null 2>&1; then
+    echo "VM already exists. Skipping."
+else
+    ~/yandex-cloud/bin/yc compute instance create \
+        --name ${name} \
+        --hostname ${name} \
+        --zone ${zone} \
+        --memory 1 \
+        --cores 1 \
+        --core-fraction 5 \
+        --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4  \
+        --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+        --ssh-key ./infra/id_rsa.pub
+fi
 }
 
 # $1 - vm name
@@ -34,15 +38,19 @@ function deploy_master {
     local ip=$4
     
 echo "Start Master ${name} VM deployment."
-~/yandex-cloud/bin/yc compute instance create \
-    --name ${name} \
-    --hostname ${name} \
-    --zone ${zone} \
-    --memory 2 \
-    --cores 2 \
-    --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4 \
-    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
-    --ssh-key ./infra/id_rsa.pub
+if ~/yandex-cloud/bin/yc compute instance get ${name} > /dev/null 2>&1; then
+    echo "VM already exists. Skipping."
+else
+    ~/yandex-cloud/bin/yc compute instance create \
+        --name ${name} \
+        --hostname ${name} \
+        --zone ${zone} \
+        --memory 2 \
+        --cores 2 \
+        --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4 \
+        --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+        --ssh-key ./infra/id_rsa.pub
+fi
 }
 
 # $1 - vm name
@@ -56,15 +64,19 @@ function deploy_node {
     local ip=$4
     
 echo "Start Node ${name} VM deployment."
-~/yandex-cloud/bin/yc compute instance create \
-    --name ${name} \
-    --hostname ${name} \
-    --zone ${zone} \
-    --memory 2 \
-    --cores 1 \
-    --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4 \
-    --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
-    --ssh-key ./infra/id_rsa.pub
+if ~/yandex-cloud/bin/yc compute instance get ${name} > /dev/null 2>&1; then
+    echo "VM already exists. Skipping."
+else
+    ~/yandex-cloud/bin/yc compute instance create \
+        --name ${name} \
+        --hostname ${name} \
+        --zone ${zone} \
+        --memory 2 \
+        --cores 1 \
+        --network-interface subnet-name=${subnet},address=${ip},nat-ip-version=ipv4 \
+        --create-boot-disk image-folder-id=standard-images,image-name=ubuntu-1604-lts-1539450374,size=20 \
+        --ssh-key ./infra/id_rsa.pub
+fi
 }
 
 deploy_master master01 ru-central1-a kube-subnet-a 192.168.10.3 
@@ -75,8 +87,4 @@ deploy_node node01 ru-central1-a kube-subnet-a 192.168.10.10
 deploy_node node02 ru-central1-b kube-subnet-b 192.168.20.10
 deploy_node node03 ru-central1-c kube-subnet-c 192.168.30.10 
 
-#deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
-
-# Find out Access VM Public IP
-#ACCESS_VM_PUBLIC_IP=$(~/yandex-cloud/bin/yc compute instance get access --format json | jq .network_interfaces[0].primary_v4_address.one_to_one_nat.address)
-#echo "Acess VM Public IP: ${ACCESS_VM_PUBLIC_IP}"
+deploy_access_vm access ru-central1-a kube-subnet-a 192.168.10.100
--- infra/yc-vpc.sh
+++ infra/yc-vpc.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-echo "Create network kube-net"
-~/yandex-cloud/bin/yc vpc network create --name kube-net
+# $1 - network name
+function create_network {
+    local name=$1
 
-echo "Create subnet kube-subnet-a"
-~/yandex-cloud/bin/yc vpc subnet create --name kube-subnet-a \
-  --network-name kube-net \
-  --zone ru-central1-a \
-  --range 192.168.10.0/24
+echo "Create network $name"
+if ~/yandex-cloud/bin/yc vpc network get ${name} > /dev/null 2>&1; then
+    echo "Network already exists. Skipping."
+else
+    ~/yandex-cloud/bin/yc vpc network create --name ${name}
+fi
+}
 
-echo "Create subnet kube-subnet-b"
-~/yandex-cloud/bin/yc vpc subnet create --name kube-subnet-b \
-  --network-name kube-net \
-  --zone ru-central1-b \
-  --range 192.168.20.0/24
+# $1 - subnet name
+# $2 - network name
+# $3 - zone
+# $4 - subnet ip range
+function create_subnet {
+    local name=$1
+    local network=$2
+    local zone=$3
+    local range=$4
 
-echo "Create subnet kube-subnet-c"
-~/yandex-cloud/bin/yc vpc subnet create --name kube-subnet-c \
-  --network-name kube-net \
-  --zone ru-central1-c \
-  --range 192.168.30.0/24  
+echo "Create subnet $name"
+if ~/yandex-cloud/bin/yc vpc subnet get ${name} > /dev/null 2>&1; then
+    echo "Subnet already exists. Skipping."
+else
+    ~/yandex-cloud/bin/yc vpc subnet create --name ${name} \
+      --network-name ${network} \
+      --zone ${zone} \
+      --range ${range}
+fi
+}
+
+create_network kube-net
+
+create_subnet kube-subnet-a kube-net ru-central1-a 192.168.10.0/24
+create_subnet kube-subnet-b kube-net ru-central1-b 192.168.20.0/24
+create_subnet kube-subnet-c kube-net ru-central1-c 192.168.30.0/24
