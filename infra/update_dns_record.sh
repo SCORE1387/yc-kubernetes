23ed45f04d36d1744065f875c55571e5e57c7e2f
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2019-02-01 22:50:43 +0300

    DNS: update DNS record with lb ip

---
+++ infra/update_dns_record.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#lb_ip=$(terraform output external_ip_address_lb01)
+lb_ip=1.1.1.2
+echo "Load Balancer Public IP: ${lb_ip}"
+
+service_name="kube"
+
+curl -X PUT \
+  https://api.godaddy.com/v1/domains/${GODADDY_DOMAIN}/records/A/${service_name} \
+  -H "Authorization: sso-key ${GODADDY_API_KEY}:${GODADDY_API_SECRET}" \
+  -H "Content-Type: application/json" \
+  -d "[
+  {
+    \"data\": \"${lb_ip}\",
+    \"name\": \"${service_name}\",
+    \"service\": \"kube\",
+    \"ttl\": 600,
+    \"type\": \"A\"
+  }
+]"
+
